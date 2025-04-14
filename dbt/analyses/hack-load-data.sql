a182312fbdf4be4a53881ce8cb42f089ae19d690
Sapient Sapiens <sapientsapiensgcp@gmail.com>
2025-04-14 17:32:49 +0000

    first dbt Cloud commit to repo

--- dbt/analyses/hack-load-data.sql
+++ dbt/analyses/hack-load-data.sql
@@ -1,49 +1,27 @@
--- MAKE SURE YOU REPLACE taxi-rides-ny-339813-412521 WITH THE NAME OF YOUR DATASET! 
--- When you run the query, only run 5 of the ALTER TABLE statements at one time (by highlighting only 5). 
--- Otherwise BigQuery will say too many alterations to the table are being made.
+-- SINCE THE DBT MODELS WERE DEVELOPED LATER AND THE HOURLY SENSORS DATA FOR THE LOCATIONS
+-- HAVE ACCUMULATED. SO WE LOAD THEM INTO THE EXTERNAL TABLE AND GET IT CONSUMED BY THE STAGING
+-- TABLE. NOTE THIS IS ONLY A ONE-TIME ACTIVITY.
 
-CREATE TABLE  `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata` as
-SELECT * FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2019`; 
+ CREATE OR REPLACE EXTERNAL TABLE `dez-capstone-project1.air_quality_assam_dataset.ext_hourly_source_data`
+      (
+          dateandtime   STRING,
+          location_id   STRING,
+          location_name STRING,
+          sensor_id     STRING,
+          sensor_name   STRING,
+          value         FLOAT64,
+          unit          STRING,
+          dlt_load_id   STRING,
+          dlt_id        STRING
+      ) 
+      OPTIONS (
+              format = 'CSV',
+              uris = ['gs://air-quality-assam-bucket/hourly_data/*.csv'],
+              skip_leading_rows = 1,
+              ignore_unknown_values = TRUE
+      );
 
 
-CREATE TABLE  `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata` as
-SELECT * FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2019`;
+ TRUNCATE TABLE `dez-capstone-project1.air_quality_assam_dataset.stg_hourly_source_data`
 
-insert into  `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata` 
-SELECT * FROM `bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2020` ;
-
-
-insert into  `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata` 
-SELECT * FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2020`; 
-
-  -- Fixes yellow table schema
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata`
-  RENAME COLUMN vendor_id TO VendorID;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata`
-  RENAME COLUMN pickup_datetime TO tpep_pickup_datetime;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata`
-  RENAME COLUMN dropoff_datetime TO tpep_dropoff_datetime;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata`
-  RENAME COLUMN rate_code TO RatecodeID;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata`
-  RENAME COLUMN imp_surcharge TO improvement_surcharge;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata`
-  RENAME COLUMN pickup_location_id TO PULocationID;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.yellow_tripdata`
-  RENAME COLUMN dropoff_location_id TO DOLocationID;
-
-  -- Fixes green table schema
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata`
-  RENAME COLUMN vendor_id TO VendorID;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata`
-  RENAME COLUMN pickup_datetime TO lpep_pickup_datetime;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata`
-  RENAME COLUMN dropoff_datetime TO lpep_dropoff_datetime;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata`
-  RENAME COLUMN rate_code TO RatecodeID;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata`
-  RENAME COLUMN imp_surcharge TO improvement_surcharge;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata`
-  RENAME COLUMN pickup_location_id TO PULocationID;
-ALTER TABLE `taxi-rides-ny-339813-412521.trips_data_all.green_tripdata`
-  RENAME COLUMN dropoff_location_id TO DOLocationID;
+ INSERT INTO `dez-capstone-project1.air_quality_assam_dataset.stg_hourly_source_data`......
